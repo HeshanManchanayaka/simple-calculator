5e8ca6b19b50c710c14a92e5c5e31eb16d7899fe
HeshanManchanayaka <hdmanchanayaka@gmail.com>
2024-11-01 12:39:19 +0530

    complete without css

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-function calc(){
-    let number01=new Number(document.getElementById("number01").value);
-    let number02=new Number(document.getElementById("number02").value);
-    let lblOutput=document.getElementById("output");
+function calc() {
+  let number01 = new Number(document.getElementById("number01").value);
+  let number02 = new Number(document.getElementById("number02").value);
+  let lblOutput = document.getElementById("output");
+  let op = document.getElementById("op").value;
 
-
-
-    lblOutput.innerHTML=(number01+number02);
+  switch (op) {
+    case "+":
+      lblOutput.innerHTML = number01 + number02;
+      console.log(number01+number02);
+      break;
+    case "-":
+      lblOutput.innerHTML = number01 - number02;
+      console.log(number01-number02);
+      break;
+    case "*":
+      lblOutput.innerHTML = number01 * number02;
+      console.log(number01*number02);
+      break;
+    case "/":
+      lblOutput.innerHTML = number01 / number02;
+      console.log(number01/number02);
+      break;
+  }
 }
